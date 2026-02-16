955ad64f3efa3daa97b24ef7c2c8dd01202d0743
jsachdev07 <48942130+jsachdev07@users.noreply.github.com>
2026-02-16 22:37:07 +0530

    Update emailNotification.groovy

--- vars/emailNotification.groovy
+++ vars/emailNotification.groovy
@@ -1,6 +1,6 @@
 def successEmail() {
     emailext(
-        to: 'himanshusachdevpimr1517@gmail.com',
+        to: 'shankarbhairappanavar@gmail.com',
         subject: "SUCCESS: ${env.JOB_NAME} #${env.BUILD_NUMBER}",
         body: "Build succeeded. Check console: ${env.BUILD_URL}"
     )
@@ -8,7 +8,7 @@ def successEmail() {
 
 def failureEmail() {
     emailext(
-        to: 'himanshusachdevpimr1517@gmail.com',
+        to: 'shankarbhairappanavar@gmail.com',
         subject: "FAILED: ${env.JOB_NAME} #${env.BUILD_NUMBER}",
         body: "Build failed. Check console: ${env.BUILD_URL}"
     )
